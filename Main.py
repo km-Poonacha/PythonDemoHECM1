7c3500eff9e1fe63b390c60dbbc8a853c8a6aa40
Poonacha Medappa <km.poonacha@gmail.com>
2016-07-21 02:21:57 +0200

    Get status from realDonaldTrump

--- Main.py
+++ Main.py
@@ -8,7 +8,15 @@ consumer_secret = 'fct2RwHn7FQNl01xh45fH9T4QxLDyve7O4WURZzf5XFxyOVcVj'
 access_token = '90844107-eW3IeYLpOY58rJkDyuVd85ZFxEHqTxljaFuPEsQnk'
 access_secret = 'gZuRhIwSio4ZcnEvPGmw9IkhofJhoLFXs8PE8UDAG97yY'
  
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
+
+def main():
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)  
+    api = tweepy.API(auth)
+
+# Get the tweets from realDonaldTrump
+    for status in tweepy.Cursor(api.user_timeline, id= 'realDonaldTrump').items(10):
+    # Process a single status
+        print(status._json) 
+
+main()        
