03c75f63927e2b64b04f9d6c2de94942f708af3e
Poonacha Medappa <km.poonacha@gmail.com>
2016-09-20 09:58:28 +0200

    This is  large commit that includes all changes made during the first week of course work

--- Session 1 code for twitter analysis.py
+++ Session 1 code for twitter analysis.py
@@ -1,3 +1,5 @@
+#The following link details how we can get twitter archive data using the tweepy  https://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/ 
+
 #Installations required -  $pip install tweepy, 
 #Installed $ pip install yahoo-finance. However before i could do it i needed to export $ CC=gcc
 # Initialize tweepy and authorize
@@ -14,17 +16,18 @@ from datetime import date
 from datetime import datetime
 from datetime import time
 
+# The following consumer ker and secret is my twitter feeds. you need to generate your consumer key and secrets for your twitter account. This is part of the OAuth security feature thatmost APIs use. 
+# you should be able to get enough documentation online to do this 
 consumer_key = 'SKi8LqcB7sxQOEXfZwPzZuBWG'
 consumer_secret = 'fct2RwHn7FQNl01xh45fH9T4QxLDyve7O4WURZzf5XFxyOVcVj'
 access_token = '90844107-eW3IeYLpOY58rJkDyuVd85ZFxEHqTxljaFuPEsQnk'
 access_secret = 'gZuRhIwSio4ZcnEvPGmw9IkhofJhoLFXs8PE8UDAG97yY'
 
-DONTWEETS_CSV = '/Users/medapa/Dropbox/HEC/Teaching/Python Sep 2016/Data/DONTWEETS.csv'
-DONTWEETS_WORDS_CSV = '/Users/medapa/Dropbox/HEC/Teaching/Python Sep 2016/Data/DONTWEETSWRDS.csv'
+DONTWEETS_CSV = '/Users/medapa/Dropbox/HEC/Teaching/Python Sep 2016/Data/DONTWEETS.csv' #<< You will need to edit this link to point it to your CSV file>>
+DONTWEETS_WORDS_CSV = '/Users/medapa/Dropbox/HEC/Teaching/Python Sep 2016/Data/DONTWEETSWRDS.csv' # This file is used to calculate the word count for all the tweets
 
 def get_index(index):
-#This function searches yahoo finance and gets the index values 
-#Trump anounced his candidacy for president on June 16 2015. So we start the tweet analysis fro mthe 16th. 
+    #"""This function searches yahoo finance and gets the index values. Trump anounced his candidacy for president on June 16 2015. So we start the tweet analysis fro mthe 16th.""" 
     start = datetime(2015,06,16)
     end = date.today()
     index_data = web.DataReader(str(index), 'yahoo', start, end)
@@ -32,22 +35,20 @@ def get_index(index):
     
     
 def collect_tweets(no_tweets):
-#This function finds the donald trump tweets and stores the collected data in a CSV file
+    #"""This function finds the donald trump tweets and stores the collected data in a CSV file. It also finds the financial index data from yahoo-finance"""
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)  
     api = tweepy.API(auth)
 #get index data and store in respective lists 
     DOW30 = get_index("^DJI")
     NASDAQ = get_index("^IXIC") 
-#    FTSE100 = get_index("^FTSE")
     CAC40 = get_index("^FCHI")
-#    EURO50 = get_index("^STOXX50E")
     MEXBOL = get_index("^MXX")   
     prev_tweet_date = ''
     tweet_count = 1
 # Get the tweets from realDonaldTrump
     for status in tweepy.Cursor(api.user_timeline, id= 'realDonaldTrump').items(no_tweets):
-#Encode to utf8
+#Encode to utf8. This is done to avoid some errors that emerge when the data in the tweets have characters that are not utf8
         if status._json['text'] is None:
             tweet_text = ''                    
         else: 
@@ -55,11 +56,11 @@ def collect_tweets(no_tweets):
         
         reply_uid = status._json['in_reply_to_user_id'] 
         reply_stateid = status._json['in_reply_to_status_id']
-#Reformat tweet date using datetime
+#Reformat tweet date using datetime. We need convert the date of the tweet to a format that CSV files can understand well 
         date1 =  str(status._json['created_at'])        
         tweet_date = datetime.strptime(date1[:20]+date1[26:30],'%a %b %d %H:%M:%S %Y')  
         
-# Get the stock index data specific to the tweet date       
+# Get the stock index data specific to the tweet date. Also note that stock index data is not available for sat and sun      
         if (date1[:3] != 'Sun' and date1[:3] != 'Sat') and (str(tweet_date.date()) != prev_tweet_date):
             print date1[:3] 
             prev_tweet_date = str(tweet_date.date())
@@ -77,25 +78,13 @@ def collect_tweets(no_tweets):
             except:
                 print "EXCEPTION KEY ERROR AT NASDAQ_val date",str(tweet_date.date())
                 NASDAQ_val = ''
-                
-#            try:
-#                FTSE100_val = FTSE100.ix[str(tweet_date.date())]
-#            except:
-#                print "EXCEPTION KEY ERROR AT FTSE100_val date",str(tweet_date.date())
-#                FTSE100_val = '' 
-                
+             
             try: 
                 CAC40_val = (CAC40.Close[str(tweet_date.date())] - CAC40.Open[str(tweet_date.date())])/CAC40.Open[str(tweet_date.date())]
             except:
                 print "EXCEPTION KEY ERROR AT CAC40_val date",str(tweet_date.date())
                 CAC40_val = ''
-                
-#            try: 
-#                EURO50_val= (EURO50.Close[str(tweet_date.date())] - EURO50.Open[str(tweet_date.date())])/EURO50.Open[str(tweet_date.date())]
-#            except:
-#                print "EXCEPTION KEY ERROR AT EURO50_val date",str(tweet_date.date())
-#                EURO50_val = ''
-            
+                         
             try:
                 MEXBOL_val = (MEXBOL.Close[str(tweet_date.date())] -MEXBOL.Open[str(tweet_date.date())])/MEXBOL.Open[str(tweet_date.date())]
             except:
@@ -107,9 +96,7 @@ def collect_tweets(no_tweets):
         else:
             DOW30_val = ''
             NASDAQ_val = ''
-#            FTSE100_val = ''
             CAC40_val = ''
- #           EURO50_val= ''
             MEXBOL_val = ''
             day_tweet_count = ''
             tweet_count = tweet_count + 1
@@ -123,7 +110,7 @@ def collect_tweets(no_tweets):
     return
     
 def word_count(word): 
-# This function counts the number of times a word has been used 
+    #"""This function counts the number of times a word has been used""" 
     with open(DONTWEETS_CSV , 'rb') as don_tweets:
         tweet_read = csv.reader(don_tweets)
         no_words = 0
@@ -137,8 +124,7 @@ def word_count(word):
         return no_words
 
 def word_index():
-# This function finds the words used and its count and stores them in a CSV
- 
+    #"""This function finds all the words used and its count and stores them in a CSV"""
     with open(DONTWEETS_CSV , 'rb') as don_tweets:
         tweet_read = csv.reader(don_tweets)
         counting_words = [''] 
@@ -165,11 +151,14 @@ def word_index():
         
                                   
 def main():        
-# mention the number of tweets required
+    #"""This is the main function that calls the relevant functions and collect the data required. """
     tweets = 15000
-#    collect_tweets(tweets)
+# The following function collects the tweets and financial index data
+    collect_tweets(tweets)
+# The following function identifies the count of the individual words used in the tweets
+    word_index()
+# You can also individually calculate the the word count for a particular word    
     print word_count('you')
-#    word_index()
 
 if __name__ == '__main__':
   main()
